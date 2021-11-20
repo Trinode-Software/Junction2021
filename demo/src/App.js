f141973614a6e489d5783ff2084f688fd723c21c
Miikka Kirsilä <miikka.kirsila@trinode.fi>
2021-11-20 21:43:06 +0200

    Demo improvements

--- demo/src/App.js
+++ demo/src/App.js
@@ -12,15 +12,57 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import Home from '@mui/icons-material/Home';
 import Chart from "react-apexcharts";
 import { useState, useEffect } from 'react'
+import Room from './components/room'
+import axios from 'axios'
 
 function App() {
 
   const [showContent, setShowContent] = useState(false)
+  const [currentTime, setCurrentTime] = useState(new Date(2021, 7, 22, 19, 58, 20, 0))
+  // const [currentTime, setCurrentTime] = useState("2021-07-22 19:58:20.233518")
+  const [data, setData] = useState({})
+  const [timeseries, setTimeseries] = useState([])
+
+  const toDateString = (date) => {
+    const toTwoDigit = (string) => {
+      return string.toString().length === 1 ? '0' + string : string
+    }
+
+    return `${date.getFullYear()}-${toTwoDigit(date.getMonth())}-${toTwoDigit(date.getDate())} ${toTwoDigit(date.getHours())}:${toTwoDigit(date.getMinutes())}:20.233518`
+  }
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowContent(true)}, 5000);
+      setShowContent(true)}, 3000);
   }, []);
+
+  const getEventAmount = async (currenttime) => {
+    return (await axios.post('https://helvar-api.herokuapp.com/realtime_rooms', {
+      site: "site_1",
+      currenttime,
+      timespan: "3600"
+    })).data.data.count
+  }
+
+  useEffect(async () => {
+    setData((await axios.post('https://helvar-api.herokuapp.com/realtime_rooms', {
+      site: "site_1",
+      currenttime: toDateString(currentTime)
+    })).data.data.count)
+    Promise.all([
+      getEventAmount(toDateString(new Date(currentTime.getTime() - (24*60*60*1000)))),
+      getEventAmount(toDateString(new Date(currentTime.getTime() - (2*24*60*60*1000)))),
+      getEventAmount(toDateString(new Date(currentTime.getTime() - (3*24*60*60*1000)))),
+      getEventAmount(toDateString(new Date(currentTime.getTime() - (4*24*60*60*1000))))
+    ]).then(function (results) {
+      setTimeseries([
+        Object.values(results[0]).reduce( (a,b) => a+b, 0),
+        Object.values(results[1]).reduce( (a,b) => a+b, 0),
+        Object.values(results[2]).reduce( (a,b) => a+b, 0),
+        Object.values(results[3]).reduce( (a,b) => a+b, 0)
+      ])
+    })
+  }, [])
 
   const options = {
     chart: {
@@ -30,7 +72,7 @@ function App() {
       }
     },
     xaxis: {
-      categories: ["16:00", "17:00", "18:00", "19:00"],
+      categories: [`${currentTime.getHours()-4}:00`, `${currentTime.getHours()-3}:00`, `${currentTime.getHours()-2}:00`, `${currentTime.getHours()-1}:00`],
       axisBorder: {
         show: false
       },
@@ -56,9 +98,10 @@ function App() {
   const series = [
     {
       name: "series-1",
-      data: [30, 12, 0, 4]
+      data: timeseries
     }
   ]
+
   return (
     <div className="App">
       {showContent ?
@@ -68,7 +111,7 @@ function App() {
         </div>
         <img src={logo} className="image"/>
         <Row className="text-center pb-3 minus-p">
-          <h1>19:14</h1>
+          <h1>{currentTime.getHours() + ':' + currentTime.getMinutes()}</h1>
         </Row>
         <Row className="text-center text-success minus-p">
           <p><span><img className="circle blink" src={circle} /></span><b>You are at home</b></p>
@@ -95,15 +138,7 @@ function App() {
               <p>Last updated</p>
             </Col>
             <Col xs={6} className="text-end">
-              <h5>19:14</h5>
-            </Col>
-          </Row>
-          <Row className="pt-2 px-3">
-            <Col xs={6}>
-              <p>Last movement</p>
-            </Col>
-            <Col xs={6} className="text-end">
-              <h5>19:14</h5>
+              <h5>{currentTime.getHours() + ':' + currentTime.getMinutes()}</h5>
             </Col>
           </Row>
           <div className="mixed-chart">
@@ -113,68 +148,12 @@ function App() {
         <Row className="px-4">
           <p className="mb-2"><b>Your rooms</b></p>
         </Row>
-        <Row className="px-3"> 
-          <div className="active-room mb-2">
-            <div className="half">
-              Kitchen
-            </div>
-            <div className="other-half">
-              <img className="circle-room blink" src={circle} />
-              <div style={{display:"inline-block"}} className="text-success small">Active now</div>
-            </div>
-          </div> 
-        </Row>
-        <Row className="px-3"> 
-          <div className="active-room mb-2">
-            <div className="half">
-              Bathroom
-            </div>
-            <div className="other-half">
-              <img className="circle-room blink" src={circle} />
-              <div style={{display:"inline-block"}} className="text-success small">Active now</div>
-            </div>
-          </div> 
-        </Row>
-        <Row className="px-3"> 
-          <div className="room mb-2">
-            <div className="half">
-              Livingroom
-            </div>
-            <div className="other-half">
-              <div className="text-room small">Active 14min ago</div>
-            </div>
-          </div> 
-        </Row>
-        <Row className="px-3"> 
-          <div className="room mb-2">
-            <div className="half">
-              Corridor
-            </div>
-            <div className="other-half">
-              <div className="text-room small">Active 18min ago</div>
-            </div>
-          </div> 
-        </Row>
-        <Row className="px-3"> 
-          <div className="room mb-2">
-            <div className="half">
-              Bedroom 1
-            </div>
-            <div className="other-half">
-              <div className="text-room small">Active 1h ago</div>
-            </div>
-          </div> 
-        </Row>
-        <Row className="px-3"> 
-          <div className="room mb-3">
-            <div className="half">
-              Bedroom 2
-            </div>
-            <div className="other-half">
-              <div className="text-room small">Active 11h ago</div>
-            </div>
-          </div> 
-        </Row>
+        <Room name='Workspace East' id={0} active={data && data["0"] && data["0"] > 0} />
+        <Room name='Workspace South' id={1} active={data && data["1"] && data["1"] > 0} />
+        <Room name='Human Resources' id={2} active={data && data["2"] && data["2"] > 0} />
+        <Room name='Grouproom "Tokyo"' id={3} active={data && data["3"] && data["3"] > 0} />
+        <Room name='Grouproom "Berlin"' id={4} active={data && data["4"] && data["4"] > 0} />
+        <Room name='Marketing' id={5} active={data && data["5"] && data["5"] > 0} />
         <Row className="px-4">
           <p className="mb-2"><b>Set timer</b></p>
         </Row>
